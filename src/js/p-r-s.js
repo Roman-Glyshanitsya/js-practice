31660c9be5fe8b408303db3d1e56a9e4126f9915
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-04-25 17:51:21 +0300

    update

---
+++ src/js/p-r-s.js
@@ -0,0 +1,3 @@
+const choiceArray = ['stone', 'scessors', 'paper'];
+const computerRandomNumber = Math.floor(Math.random() * 3 + 1);
+const computerChoice = choiceArray[computerRandomNumber];
--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import './js/dark-theme';
 import './js/birth-year';
 import './js/guess-number';
+import './js/p-r-s';
 import './js/dinosaur';
