1130c18fe5a6a1c3a34809d5a3b2bace62344ed0
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-04-29 03:09:48 +0300

    update

--- src/js/scientest.js
+++ src/js/scientest.js
@@ -1,8 +1,7 @@
 import { scientists } from '../data/scientist-data';
 
-console.log(scientists);
-
 const sceintistGalleryList = document.getElementById('scientist-gallery');
+const sceintistBtnsList = document.querySelectorAll('.scientist__button');
 
 function createsScientistGalary(scientists) {
   return scientists
@@ -18,7 +17,21 @@ function createsScientistGalary(scientists) {
     .join('');
 }
 
-sceintistGalleryList.insertAdjacentHTML(
-  'afterbegin',
-  createsScientistGalary(scientists)
-);
+const filtered = scientists
+  .filter(({ born }) => born >= 1800 && born < 1900)
+  .map(({ name, surname, image, born, dead, id }) => {
+    return `
+      <li id="${id}" class="scientist__item" style="background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0), rgba(0, 0, 0, 0.7)), url('${image}');">
+      <div class="scientist__text-thumb">
+      <p>${name} ${surname}</p>
+      <p>${born}-${dead}</p>
+      </div>
+      </li>`;
+  })
+  .join('');
+
+// //  Які вчені народилися в 19 ст.
+
+sceintistBtnsList[0].addEventListener('click', () => {
+  sceintistGalleryList.insertAdjacentHTML('afterbegin', filtered);
+});
