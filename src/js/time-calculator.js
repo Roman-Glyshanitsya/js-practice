1bc8095d361a10ccc8ed8ff89e26fd193950d43a
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-05-02 00:42:30 +0300

    update

--- src/js/time-calculator.js
+++ src/js/time-calculator.js
@@ -8,11 +8,23 @@ function onBtnClick(e) {
   e.preventDefault();
 
   const days = Math.floor(form.usernumber.value / 60 / 24);
-  console.log('days', days);
   const hourse = Math.floor((form.usernumber.value / 60) % 24);
-  console.log('hourse', hourse);
   const minutes = form.usernumber.value % 60;
-  console.log('minutes', minutes);
 
-  result.textContent = `${days} дн. ${hourse}:${minutes}:00`;
+  if (form.usernumber.value <= 0) {
+    result.textContent = 'Введіть додатнє число';
+    result.style.color = 'red';
+  } else if (hourse <= 9 && minutes <= 9) {
+    result.textContent = `${days} дн. 0${hourse}:0${minutes}:00`;
+    result.style.color = 'var(--secondary-color)';
+  } else if (hourse <= 9) {
+    result.textContent = `${days} дн. 0${hourse}:${minutes}:00`;
+    result.style.color = 'var(--secondary-color)';
+  } else if (minutes <= 9) {
+    result.textContent = `${days} дн. ${hourse}:0${minutes}:00`;
+    result.style.color = 'var(--secondary-color)';
+  } else {
+    result.textContent = `${days} дн. ${hourse}:${minutes}:00`;
+    result.style.color = 'var(--secondary-color)';
+  }
 }
