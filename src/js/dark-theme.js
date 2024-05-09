c39573b19d56b87df0fb17d7a0f25d2da6f4bcfc
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-05-09 14:41:14 +0300

    update

--- src/js/dark-theme.js
+++ src/js/dark-theme.js
@@ -4,8 +4,8 @@ icon.addEventListener('click', () => {
   document.body.classList.toggle('dark-theme');
 
   if (document.body.classList.contains('dark-theme')) {
-    icon.src = '/dark-theme-icon.svg';
+    icon.src = 'img/icons/dark-theme-icon.svg';
   } else {
-    icon.src = '/light-theme-icon.svg';
+    icon.src = 'img/icons/light-theme-icon.svg';
   }
 });
