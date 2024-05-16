1001357a0df8063d6c2c92051b3f63a8e448898f
Roman-Glyshanitsya <r.glyshanitsya@gmail.com>
2024-05-16 15:07:30 +0300

    update

--- src/js/calculator.js
+++ src/js/calculator.js
@@ -4,14 +4,47 @@ const operationBtnList = document.querySelectorAll(
   '.calculator__operation__button'
 );
 const resultBtn = document.querySelector('.calculator__result__button');
-const resultEl = document.querySelector('calculator__result');
+const resultEl = document.querySelector('.calculator__result');
 
-let operation = '';
+let operation = null;
 
 operationBtnList.forEach(btn => {
   btn.addEventListener('click', e => {
     operation = e.currentTarget.textContent;
+    // console.log(operation);
     operationBtnList.forEach(btn => btn.classList.remove('selected'));
     btn.classList.add('selected');
   });
 });
+
+resultBtn.addEventListener('click', calculate);
+
+function calculate() {
+  const firstNumber = parseFloat(firstInput.value);
+  const secondNumber = parseFloat(secondInput.value);
+
+  let result = '';
+
+  switch (operation) {
+    case '+':
+      result = firstNumber + secondNumber;
+      break;
+
+    case '-':
+      result = firstNumber - secondNumber;
+      break;
+
+    case '*':
+      result = firstNumber * secondNumber;
+      break;
+
+    case '/':
+      result = firstNumber / secondNumber;
+      break;
+
+    default:
+      result = 'Результат';
+  }
+
+  resultEl.textContent = result;
+}
